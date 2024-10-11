d237b5e0b116600aa3d9976514869cd17d0e6c42
whmcs-ru-lang <stimakkovoa@gmail.com>
2024-10-11 11:38:15 +0200

    Update cryptopay.php

--- modules/gateways/callback/cryptopay.php
+++ modules/gateways/callback/cryptopay.php
@@ -9,16 +9,16 @@ if (!defined("WHMCS")) {
 $input = file_get_contents('php://input');
 $data = json_decode($input, true);
 
-if (!isset($data['invoice_id']) || !isset($data['status'])) {
+if (!isset($data['payload']['invoice_id']) || !isset($data['payload']['status']) || $data['update_type'] != "invoice_paid") {
     http_response_code(400);
     echo 'Invalid payload';
     exit();
 }
 
-$invoiceId = $data['payload'];
-$status = $data['status'];
-$amountPaid = isset($data['paid_amount']) ? $data['paid_amount'] : null;
-$paidAsset = isset($data['paid_asset']) ? $data['paid_asset'] : null;
+$invoiceId = $data['payload']['invoice_id'];
+$status = $data['payload']['status'];
+$amountPaid = isset($data['payload']['paid_amount']) ? $data['payload']['paid_amount'] : null;
+$paidAsset = isset($data['payload']['paid_asset']) ? $data['payload']['paid_asset'] : null;
 
 logModuleCall('cryptopay', 'callback', $data, '', '', ['invoiceId' => $invoiceId, 'status' => $status]);
 
